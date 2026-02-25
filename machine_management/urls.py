95333c0fa7f4b194269a95d820f878791de8101c
fu89.developer <fu89.team@gmail.com>
2026-02-25 13:57:16 +0700

    feat: Implement initial machine management module with user list, machine detail, and home page views.

--- machine_management/urls.py
+++ machine_management/urls.py
@@ -15,15 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.shortcuts import render
 
 from . import views
 
+def page_not_found(request, *args, **kwargs):
+    return render(request, 'page_404.html', status=404)
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('machine-detail/', views.machine_detail, name='machine-detail'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('authentication/', include('authentication.urls')),
+    
+    re_path(r'^.*$', page_not_found, name='page_not_found'),
 ]
 
 handler404 = 'machine_management.views.custom_404'
--- machine_management/views.py
+++ machine_management/views.py
@@ -4,13 +4,16 @@ from django.db.models import Max
 from accounts.models import Machine_Logs
 
 @login_required(login_url="/authentication/login")
-
 def home(request):
     max_id = Machine_Logs.objects.aggregate(max_id=Max('id'))['max_id']
     context = {
         'max_id': max_id
     }
     return render(request, "home.html", context)
+
+@login_required(login_url="/authentication/login")
+def machine_detail(request):
+    return render(request, "machine_detail.html")
 
 def custom_404(request, exception):
     return render(request, 'page_404.html', status=404)
